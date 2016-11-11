28a3a8f8db5ab6c17d46de52a2af7d14480c7ec0
chenfei1 <chenfei1@7F-Chenfei1.globale.com>
2016-11-11 14:47:25 +0800

    提交代码

--- allen-commons-exception/src/main/java/com/allen/commons/exception/filter/SimpleExceptionHandlerFilter.java
+++ allen-commons-exception/src/main/java/com/allen/commons/exception/filter/SimpleExceptionHandlerFilter.java
@@ -9,25 +9,34 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class SimpleExceptionHandlerFilter implements Filter {
 
+	private final static Logger logger = LoggerFactory.getLogger(SimpleExceptionHandlerFilter.class);
+	
+	
 	@Override
 	public void init(FilterConfig filterConfig) throws ServletException {
-		// TODO Auto-generated method stub
-
+		logger.info("start execute SimpleExceptionHandlerFilter.init --------------");
+		
+		logger.info("end execute SimpleExceptionHandlerFilter.init -----------------");
 	}
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
+		logger.info("start execute SimpleExceptionHanlerFilter.doFilter  -----------");
 
+		logger.info("end execute SimpleExceptionHanlerFilter.doFilter --------------");
 	}
 
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
-
+		logger.info("start execute SimpleExceptionHanlderFilter.distory -------------");
+		
+		logger.info("end execute SimipleExceptionHanlderFilter.distory -------------- ");
 	}
 
 }
